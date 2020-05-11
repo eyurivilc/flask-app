55e1097ed3011b06a18cf6e29702a6dc3b6aca0f
Emerson YR <eyurivilcar@gmail.com>
2020-05-11 11:34:15 -0500

    make login and logout connected to firestore

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,9 @@
 from flask import render_template, session, redirect, flash, url_for
+from flask_login import login_user, logout_user, login_required
 from app.forms import LoginForm
 from . import auth
+from app.firestore_service import get_user
+from app.models import UserModel, UserData
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -10,11 +13,38 @@ def login():
     }
 
     if login_form.validate_on_submit():
-        username = login_form.username.data
-        session['username'] = username
 
-        flash('Nombre de usuario registrado con éxito!')
+        username = login_form.username.data
+        password = login_form.password.data
+        user_doc = get_user(username)
+
+        if user_doc.to_dict() is not None:
+            password_from_db = user_doc.to_dict()['password']
+
+            if password == password_from_db:
+                user_data = UserData(username, password)
+                user = UserModel(user_data)
+
+                login_user(user)
+
+                flash('Bienvenido de nuevo')
+                
+                redirect(url_for('hello'))
+            else:
+                flash('La información no coincide')
+        else:
+            flash('El usuario no existe')
+
 
         return redirect(url_for('index'))
 
     return render_template('login.html', **context)
+
+
+@auth.route('logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Regresa pronto')
+
+    return redirect(url_for('auth.login'))
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from flask import (make_response, redirect, render_template, request,
                    session, url_for, flash)
+from flask_login import login_required, current_user
 import unittest
 from app import create_app
 from app.forms import LoginForm
+from app.firestore_service import get_users, get_todos
 
 app = create_app()
-
-todos = ['Comprar café', 'Enviar solicitud de compra', 'Entregar video a productor']
 
 
 @app.cli.command()
@@ -36,13 +36,13 @@ def index():
 
 
 @app.route('/hello', methods=['GET'])
+@login_required
 def hello():
     user_ip = session.get('user_ip')
-    username = session.get('username')
+    username = current_user.id
     context = {
         'user_ip': user_ip,
-        'todos': todos,
-        #'login_form': login_form,
+        'todos': get_todos(user_id=username),
         'username': username,
     }
 
